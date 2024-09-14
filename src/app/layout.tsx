f81c3c6daac3e31c40ae7bfe03393234cb8e1946
iamsnehadas <snehasnow8@gmail.com>
2024-09-14 18:24:59 +0530

    Updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import { Analytics } from "@vercel/analytics/react"
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || '');
 
